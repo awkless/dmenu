cea6cebd700db72edeac176cd4f59f544c466bbb
Jason Pena <jasonpena@awkless.com>
2023-05-17 16:45:00 -0600

    feat(grid): Add grid and gridnav patches
    Signed-off-by: Jason Pena <jasonpena@awkless.com>

--- config.h
+++ config.h
@@ -3,7 +3,7 @@
 
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 static int centered = 1;                    /* -c option; centers dmenu on screen */
-static int min_width = 500;                    /* minimum width when centered */
+static int min_width = 1000;                    /* minimum width when centered */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
 	"Hurmit Nerd Font:size=12"
@@ -15,8 +15,9 @@ static const char *colors[SchemeLast][2] = {
 	[SchemeSel] = { "#eeeeee", "#005577" },
 	[SchemeOut] = { "#000000", "#00ffff" },
 };
-/* -l option; if nonzero, dmenu uses vertical list with given number of lines */
-static unsigned int lines      = 0;
+/* -l and -g options; controls number of lines and columns in grid if > 0 */
+static unsigned int lines      = 5;
+static unsigned int columns    = 5;
 
 /*
  * Characters not considered part of a word while deleting words
