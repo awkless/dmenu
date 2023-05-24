d922705721da31bbb6c507cdfac805a3c6a195eb
Jason Pena <jasonpena@awkless.com>
2023-05-23 21:32:44 -0600

    feat(config.h): Add srcery colorscheme to dmenu
    Signed-off-by: Jason Pena <jasonpena@awkless.com>

--- config.h
+++ config.h
@@ -7,16 +7,17 @@ static int centered = 1;                    /* -c option; centers dmenu on scree
 static int min_width = 1000;                    /* minimum width when centered */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"Hurmit Nerd Font:size=12"
+	"Hurmit Nerd Font:style=Regular:size=12"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
+
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#eeeeee", "#005577" },
-	[SchemeSelHighlight] = { "#ffc978", "#005577" },
-	[SchemeNormHighlight] = { "#ffc978", "#222222" },
-	[SchemeOut] = { "#000000", "#00ffff" },
+	[SchemeNorm] = { "#fce8c3", "#1c1b19" },
+	[SchemeSel] = { "#f75341", "#3a3a3a" },
+	[SchemeSelHighlight] = { "#98bc37", "#3a3a3a" },
+	[SchemeNormHighlight] = { "#98bc37", "#1c1b19" },
+	[SchemeOut] = { "#0aaeb3", "#1c1b19" },
 };
 /* -l and -g options; controls number of lines and columns in grid if > 0 */
 static unsigned int lines      = 5;
